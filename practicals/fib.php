f0af63fd368638c3564be1c1f89258db8a58ff8c
Ayush_Ubuntu <ayushsachan49@gmail.com>
2023-10-31 20:41:27 +0530

    Revised the fibonacci php code to make it more readable and maintable

--- practicals/fib.php
+++ practicals/fib.php
@@ -1,15 +1,17 @@
 <?php
+// Function to calculate the nth Fibonacci number recursively
 function fibonacci($n) {
+    // Base cases: Fibonacci of 0 or 1 is the number itself
     if ($n <= 1) {
         return $n;
     } else {
+        // Recursive case: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
         return fibonacci($n - 1) + fibonacci($n - 2);
     }
 }
-
-// Example: Print the first 10 Fibonacci numbers
-for ($i = 0; $i < 10; $i++) {
-    echo fibonacci($i) . ' ';
+$x=10; //Number of terms in fibonacci series
+// Example: Print the first x Fibonacci numbers
+for ($i = 0; $i < $x; $i++) {
+    echo fibonacci($i) . ' '; // Output each Fibonacci number separated by space
 }
-?>
 
