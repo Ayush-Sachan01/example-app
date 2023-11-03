568f8b2ac427b6a6932236656f7db7ac698e1576
Ayush_Ubuntu <ayushsachan49@gmail.com>
2023-11-03 10:06:00 +0530

    Added a php class named Song with getter and setter functions

---
+++ practicals/song.php
@@ -0,0 +1,62 @@
+<?php
+
+class Song {
+    //Following are various properties of Song
+    private $title;
+    private $artist;
+    private $genre;
+    private $tempo;
+
+    // Getter and setter for title
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    // Getter and setter for artist
+    public function getArtist() {
+        return $this->artist;
+    }
+
+    public function setArtist($artist) {
+        $this->artist = $artist;
+    }
+
+    // Getter and setter for genre
+    public function getGenre() {
+        return $this->genre;
+    }
+
+    public function setGenre($genre) {
+        $this->genre = $genre;
+    }
+
+    // Getter and setter for tempo
+    public function getTempo() {
+        return $this->tempo;
+    }
+
+    public function setTempo($tempo) {
+        $this->tempo = $tempo;
+    }
+}
+
+// Creating an instance of the Song class
+$track1 = new Song();
+
+// Setting values using setter methods
+$track1->setTitle("ABC SONG");
+$track1->setArtist("Eminem");
+$track1->setGenre("Jazz");
+$track1->setTempo("Moderato");
+
+// Displaying information
+echo "Title of the song is: {$track1->getTitle()} \n";
+echo "Artist of the song is: {$track1->getArtist()} \n";
+echo "Genre of the song is: {$track1->getGenre()} \n";
+echo "Tempo of the song is: {$track1->getTempo()} \n";
+
+?>
